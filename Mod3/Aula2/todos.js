846186aeaefe53dd04b5d10f6413ef74a141f2d3
vinicius <vinicius_lunardi@estudante.sc.senai.br>
2019-06-26 15:19:31 -0300

    Aula2 mod3

---
+++ Mod3/Aula2/todos.js
@@ -0,0 +1,10 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    //{ text: 'Fazer café', propriedade: 1}
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
+];
